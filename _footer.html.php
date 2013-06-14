15df540249f80337dab7a08149dd097706e279f6
Patrick Rauscher <prauscher@ohai.su>
2013-06-14 13:51:39 +0000

    add basic files

---
+++ _footer.html.php
@@ -0,0 +1,4 @@
+    <script src="jquery.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once(dirname(__FILE__) . "/config.inc.php");
+
+include("_header.html.php");
+
+?>
+
+<?php
+include("_footer.html.php");
+?>
