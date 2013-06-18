cfe26ceac3a441a3d8c593d8d4b0687ca25a49a2
Patrick Rauscher <prauscher@ohai.su>
2013-06-18 14:58:17 +0000

    reviewed mail

--- requestAuth.mail.php
+++ requestAuth.mail.php
@@ -3,4 +3,11 @@ From: "Mitgliederverwaltung | Junge Piraten" <mitglieder@junge-piraten.de>
 
 Hallo <?php print(isset($mitglied->latest->natperson) ? $mitglied->latest->natperson->vorname : $mitglied->latest->jurperson->label) ?>,
 
+du hast einen Legetimationslink für das Mitgliederverwaltungsportal angefordert. Solltest du das nicht getan haben,
+ignoriere diese Mail bitte, ansonsten kannst du unter
+
 https://mitglieder.junge-piraten.de/?mitgliedid=<?php print($mitglied->mitgliedid) ?>&token=<?php print($token) ?>&
+
+deine Mitgliederdaten einsehen und auch bearbeiten.
+
+Viele Grüße,
